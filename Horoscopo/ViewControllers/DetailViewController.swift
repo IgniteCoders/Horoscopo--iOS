42b810693ad0b72e2b4396b7f71919c5a542619f
Mansour <theboom.kajas@gmail.com>
2024-12-16 20:00:53 +0100

    Implemented the session in the DetailViewController

--- Horoscopo/ViewControllers/DetailViewController.swift
+++ Horoscopo/ViewControllers/DetailViewController.swift
@@ -10,18 +10,44 @@ import UIKit
 class DetailViewController: UIViewController {
     
     var horoscope: Horoscope!
+    var isFavorite: Bool = false
+    var session: SessionManager!
 
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var datesLabel: UILabel!
     
+    @IBOutlet weak var favoriteMenu: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        session = SessionManager()
 
         // Do any additional setup after loading the view.
         navigationItem.title = horoscope.name
         
         datesLabel.text = horoscope.dates
         iconImageView.image = horoscope.icon
+        
+        isFavorite = session.isFavorite(horoscopeId: horoscope.id)
+        setFavoriteIcon()
+    }
+    
+    func setFavoriteIcon() {
+        if (isFavorite) {
+            favoriteMenu.image = UIImage(systemName: "heart.fill")
+        } else {
+            favoriteMenu.image = UIImage(systemName: "heart")
+        }
     }
 
+    @IBAction func setFavorite(_ sender: Any) {
+        if (isFavorite) {
+            session.setFavorite(horoscopeId: "")
+        } else {
+            session.setFavorite(horoscopeId: horoscope.id)
+        }
+        isFavorite = !isFavorite
+        setFavoriteIcon()
+    }
 }
